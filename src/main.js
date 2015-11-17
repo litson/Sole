6ebad42207f5d87e8ae25251008c2384c189f9ce
zhangtao23 <zhangtao23@baidu.com>
2015-11-17 16:37:07 +0800

    updates

--- src/main.js
+++ src/main.js
@@ -11,22 +11,23 @@
 
 'use strict';
 
-require('./modules/sole.css');
-require('./modules/agate.css');
-var Vue      = require('./modules/vue.js');
-var hljs     = require('./modules/highlight.js');
-var toArray  = require('./modules/toArray.js');
-var json2str = require('./modules/json2str.js');
-var template = require('./modules/sole.html');
+require( './modules/sole.css' );
+require( './modules/agate.css' );
+var Vue      = require( './modules/vue.js' );
+var hljs     = require( './modules/highlight.js' );
+var toArray  = require( './modules/toArray.js' );
+var json2str = require( './modules/json2str.js' );
+var template = require( './modules/sole.html' );
 
 var win  = window;
 var doc  = document;
 var sole = {
     open: true
 };
+
 var originalConsole = console;
 var overrideMethods = [];
-var methods = [
+var methods         = [
     'assert'
     , 'clear'
     , 'count'
@@ -52,76 +53,84 @@ var methods = [
 ];
 
 // filters
-Vue.filter('preview', function (value) {
+Vue.filter( 'preview', function ( value ) {
 
     var content = typeof value.content === 'string'
         ? value.content
-        : json2str(value.content);
+        : json2str( value.content );
 
     content = '<span class="debugger-text-' + value.type + '">[ ' + value.type + ' ]</span>: ' + content;
 
     return content;
-});
+} );
 
-Vue.filter('highlight', function (value) {
+Vue.filter( 'highlight', function ( value ) {
     var self = this;
 
-    setTimeout(function () {
+    // code style highlight.
+    setTimeout( function () {
         var hlElement = self
             .$forContext
             .cache[self.$index]
-            .node.getElementsByTagName('pre')[0]
-            .getElementsByTagName('code')[0];
-        hljs.highlightBlock(hlElement);
-    }, 1);
+            .node.getElementsByTagName( 'pre' )[0]
+            .getElementsByTagName( 'code' )[0];
+        hljs.highlightBlock( hlElement );
+    }, 1 );
 
     return value;
-});
+} );
 
 var $messages = null;
-var vm        = new Vue({
+
+var vm = new Vue( {
+
     data: {
         messages: []
     },
+
     methods: {
-        removeMessage: function (index) {
-            $messages.splice(index, 1);
+
+        removeMessage: function ( index ) {
+            $messages.splice( index, 1 );
         },
+
         removeAllMessage: function () {
             console.clear();
         }
     }
-});
+
+} );
 
 $messages = vm.$data.messages;
 
-function register(type, cb) {
-    overrideMethods.push(type);
+function register( type, cb ) {
+    overrideMethods.push( type );
     sole[type] = cb;
 }
 
-function messagePackage(arrayLike, type) {
+function messagePackage( arrayLike, type ) {
 
-    var messages = deepInside(arrayLike, type);
-    var args     = messages.original;
+    var messages = deepInside( arrayLike, type );
+
+    var args = messages.original;
 
     // output in dom
-    if (sole.open) {
-        $messages.push.apply($messages, messages.sole);
+    if ( sole.open ) {
+        $messages.push.apply( $messages, messages.sole );
     }
 
     // console.log sync output
-    originalConsole[type].apply(originalConsole, args);
+    originalConsole[type].apply( originalConsole, args );
 }
 
-function deepInside(arrayLike, type) {
-    var originalData = toArray(arrayLike);
+function deepInside( arrayLike, type ) {
+    var originalData = toArray( arrayLike );
     return {
         original: originalData,
-        sole: originalData.map(
-            function (item) {
+        sole    : originalData.map(
+            function ( item ) {
                 return {
-                    type: type,
+                    type   : type,
                     content: item
                 }
             }
@@ -130,39 +139,47 @@ function deepInside(arrayLike, type) {
 }
 
 // 
-var $soleDom = doc.createElement('div');
+var $soleDom = doc.createElement( 'div' );
+
 $soleDom.innerHTML = template;
-doc.body.appendChild($soleDom);
-vm.$mount('#sole');
+doc.body.appendChild( $soleDom );
+vm.$mount( '#sole' );
 
 // Inject member.
-['log', 'warn', 'info', 'error', 'dir'].forEach(function (item) {
-    register(item, function () {
-        messagePackage(arguments, item);
-    });
-});
+[
+    'log'
+    , 'warn'
+    , 'info'
+    , 'error'
+    , 'dir'
+]
+    .forEach( function ( item ) {
+        register( item, function () {
+            messagePackage( arguments, item );
+        } );
+    } );
 
-register('clear', function () {
+register( 'clear', function () {
     originalConsole.clear();
-    $messages.splice(0, $messages.length);
-});
+    $messages.splice( 0, $messages.length );
+} );
 
 // Remove members that have been overloaded.
-methods = methods.filter(function (item) {
-    return overrideMethods.indexOf(item) === -1;
-});
+methods = methods.filter( function ( item ) {
+    return overrideMethods.indexOf( item ) === -1;
+} );
 
 // Inject other members.
-methods.forEach(function (type) {
-    register(type, function () {
-        return originalConsole[type].apply(originalConsole, arguments);
-    });
-});
+methods.forEach( function ( type ) {
+    register( type, function () {
+        return originalConsole[type].apply( originalConsole, arguments );
+    } );
+} );
 
 // when error happened.
-win.addEventListener('error', function (event) {
-    console.error(event.message);
-}, false);
+win.addEventListener( 'error', function ( event ) {
+    console.error( event.message );
+}, false );
 
 // Bind in global.
 win.console  = sole;
